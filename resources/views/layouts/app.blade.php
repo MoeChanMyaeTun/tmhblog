b7ed3bc215f3175523f44df998d1e89a4483675d
Moe Chan Myae Htun <scm.moechanmyaehtun@gmail.com>
2022-12-06 16:45:58 +0630

    postcreate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,16 +46,7 @@
                                 Home
                               </a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  Post
-                               </a>
-                               <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('post.create') }}">
-                                  Post Create
-                                </a>
-                            </div>
-                       </li>
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
@@ -70,6 +61,16 @@
                                 </li>
                             @endif
                         @else
+                             <li class="nav-item dropdown">
+                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                  Post
+                                 </a>
+                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                 <a class="dropdown-item" href="{{ route('post.create') }}">
+                                  Post Create
+                                 </a>
+                                </div>
+                              </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
